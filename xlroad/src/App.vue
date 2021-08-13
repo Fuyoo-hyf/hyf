301e1f07bba9572c5533f7045d1cd2dddc815792
Fuyoo-hyf <986785700@qq.com>
2021-08-13 10:25:41 +0800

    卡片demo

--- xlroad/src/App.vue
+++ xlroad/src/App.vue
@@ -2,14 +2,17 @@
   <div class="win">
     <!-- 第一人称操作 -->
     <div class="plantplom flex">
-      <div class="flex_a"></div>
+      <div class="flex_a">
+        <div class="card fl"></div>
+        <div class="card fl"></div>
+      </div>
       <div class="card flexone">
-      <img src="./assets/demopeo.png" alt="">
+        <img src="./assets/demopeo.png" alt="" />
+      </div>
     </div>
-    </div>
-    
+
     <div class="card p2">
-      <img src="./assets/demopeo.png" alt="">
+      <img src="./assets/demopeo.png" alt="" />
     </div>
   </div>
 </template>
@@ -109,7 +112,7 @@ export default {
   height: 100vh;
 }
 
-.plantplom{
+.plantplom {
   position: fixed;
   width: 100%;
   height: 150px;
@@ -118,26 +121,30 @@ export default {
   box-sizing: border-box;
 }
 
-.flex{
+.flex {
   display: flex;
 }
 
-.flex_a{
+.fl {
+  float: left;
+}
+
+.flex_a {
   flex: 10;
 }
 
-.flexone{
+.flexone {
   flex: 1;
 }
 
-.card{
+.card {
   width: 9vw;
   height: 150px;
   border: 1px solid black;
   box-sizing: border-box;
 }
 
-.p1{
+.p1 {
   position: fixed;
   /* left: 0; */
   right: 0;
@@ -145,7 +152,7 @@ export default {
   /* margin: auto; */
 }
 
-.p2{
+.p2 {
   position: fixed;
   left: 0;
   right: 0;
@@ -153,7 +160,7 @@ export default {
   margin: auto;
 }
 
-img{
+img {
   width: 100%;
   height: 100%;
 }
